b02bc39362e71d27fca96caf60e9321bd5c1df87
TomasEscobar1 <thomasatlascorporation@gmail.com>
2024-11-24 18:06:32 -0500

    Prueba 1 Dofi

--- main.js
+++ main.js
@@ -12,12 +12,14 @@ renderer.setSize(w, h);
 const scene = new THREE.Scene();
 const axesHelper = new THREE.AxesHelper(7);
 scene.add(axesHelper);
+renderer.shadowMap.enabled = true;
+scene.background = new THREE.Color(0x9ad6ce);
 
 document.body.appendChild(renderer.domElement);
 
 const GLoader = new GLTFLoader();
 
-GLoader.load("assets/tele_25_inch_assembly.glb", function (gltf) {
+GLoader.load("assets/modular_desk.glb", function (gltf) {
   const model = gltf.scene;
 
   scene.add(model);
@@ -25,21 +27,29 @@ GLoader.load("assets/tele_25_inch_assembly.glb", function (gltf) {
   model.translateZ(-0.3);
   model.translateX(0.0);
   model.rotateX(0.0);
-  /*gltf.animations;
+  
+  gltf.animations;
   gltf.scene;
   gltf.scenes;
   gltf.cameras;
+  model.traverse(function(node)
+  {
+    if(node.isMesh)
+    {
+      node.castShadow = true;
+    }
+  });
+
   gltf.asset;}, 
     function(xhr) {console.log(( xhr.loaded/xhr.total * 100 ) + '%loaded');},
-    function(error) {console.log('An error happened');}*/
-  }
+    function(error) {console.log('An error happened');}
 );
 
 const paragraph = document.createElement('paragraph');
-paragraph.textContent = 'Hello world!!';
+paragraph.textContent = 'Esta es una prueba para Industrias Dofi';
 const cPointLabel = new CSS2DObject(paragraph);
 scene.add(cPointLabel);
-cPointLabel.position.set(0, 0.5, 0);
+cPointLabel.position.set(0, 2.5, 0);
 
 const labelRenderer = new CSS2DRenderer();
 labelRenderer.setSize(window.innerWidth, window.innerHeight);
@@ -48,18 +58,21 @@ labelRenderer.domElement.style.top = '0px';
 labelRenderer.domElement.style.pointerEvents = 'none';
 document.body.appendChild(labelRenderer.domElement);
 
-const planeGeo = new THREE.PlaneGeometry(6, 6, 6);
-const planeMat = new THREE.MeshPhongMaterial({color: 0x48ABE4, shininess: 1200})
+const planeGeo = new THREE.PlaneGeometry(30, 30);
+const planeMat = new THREE.MeshPhongMaterial({color: 0XDEB887, shininess: 200})
 const plane1 = new THREE.Mesh(planeGeo, planeMat);
 plane1.rotateX(Math.PI/-2);
-//scene.add(plane1);
+scene.add(plane1);
+plane1.receiveShadow = true;
 
 const aspect = w/h;
 const fov = 50;
 const near = 0.1;
-const far = 10;
+const far = 50;
 const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 2;
+camera.position.z = 3;
+camera.position.y = 4;
+camera.position.x = -5;
 
 //const scene = new THREE.Scene();
 const controls = new OrbitControls(camera, renderer.domElement);
@@ -69,16 +82,19 @@ const geo = new THREE.IcosahedronGeometry(1.0, 2);
 const mat = new THREE.MeshStandardMaterial( {color: 0xffffff, flatShading: true });
 const mesh = new THREE.Mesh(geo, mat);
 //scene.add(mesh);
+mesh.position.y = 2.0;
+mesh.castShadow = true;
 
 const wireMat = new THREE.MeshBasicMaterial( { color: 0xccff, wireframe:true } );
 const wireMesh = new THREE.Mesh(geo, wireMat);
 wireMesh.scale.setScalar(1.001);
 //mesh.add(wireMesh);
 
-const dirLight = new THREE.DirectionalLight(0xFFFFFF, 1.2);
+const dirLight = new THREE.DirectionalLight(0xFFFFFF, 2.0);
 const dirLightHelper = new THREE.DirectionalLightHelper(dirLight);
+dirLight.castShadow = true;
 dirLight.position.x = 0.5;
-dirLight.position.y = 1.1;
+dirLight.position.y = 3.1;
 dirLight.position.z = 1.3;
 dirLight.rotateY(-1.0);
 dirLight.rotateZ(-0.8);
