cfb0e0a121c0be51243863b8ff67cc234ef49df9
TomasEscobar1 <thomasatlascorporation@gmail.com>
2024-12-06 11:17:25 -0500

    Código depurado y documentado

--- main.js
+++ main.js
@@ -1,27 +1,95 @@
 
+
 import * as THREE from "three";
 import { OrbitControls, ThreeMFLoader } from "three/examples/jsm/Addons.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { CSS2DRenderer, CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer.js";
+
+
+// Setup general del programa
 
 let value1 = 0;
 let Anim_2 =[];
 
 const w = window.innerWidth;
 const h = window.innerHeight;
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(w, h);
 document.body.appendChild(renderer.domElement);
 renderer.shadowMap.enabled = true;
+renderer.setClearColor(0xB0C4DE, 0.5);
+
 const scene = new THREE.Scene();
 const axesHelper = new THREE.AxesHelper(7);
+
 let mixer = new THREE.AnimationMixer();
 //scene.add(axesHelper);
 scene.background = new THREE.Color(0XFFFFFF);
 
+const planeGeo = new THREE.PlaneGeometry(30, 30);
+const planeMat = new THREE.MeshPhongMaterial({color: 0XFFFFFF, shininess:1200})
+const plane1 = new THREE.Mesh(planeGeo, planeMat);
+plane1.rotateX(Math.PI/-2);
+scene.add(plane1);
+plane1.receiveShadow = true;
+
+const aspect = w/h;
+const fov = 50;
+const near = 0.1;
+const far = 50;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.x = -4;
+camera.position.y = 4;
+camera.position.z = 3;
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.08;
+
+/*const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshStandardMaterial( {color: 0xffffff, flatShading: true });
+const mesh = new THREE.Mesh(geo, mat);
+const wireMat = new THREE.MeshStandardMaterial( { color: 0xccff, wireframe:true, flatShading:false } );
+const wireMesh = new THREE.Mesh(geo, wireMat);
+wireMesh.scale.setScalar(1.001);
+mesh.add(wireMesh);
+scene.add(mesh);
+mesh.position.y = 2.0;
+mesh.castShadow = true;*/
+
+const dirLight = new THREE.DirectionalLight(0xFFFFFF, 2.0);
+//const dirLightHelper = new THREE.DirectionalLightHelper(dirLight);
+dirLight.castShadow = true;
+dirLight.position.x = 0.5;
+dirLight.position.y = 3.1;
+dirLight.position.z = 1.3;
+dirLight.rotateY(-1.0);
+dirLight.rotateZ(-0.8);
+scene.add(dirLight);
+//scene.add(dirLightHelper);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff);
+scene.add(hemiLight);
+
+const labelRenderer = new CSS2DRenderer();
+labelRenderer.setSize(window.innerWidth, window.innerHeight);
+labelRenderer.domElement.style.position = 'absolute';
+labelRenderer.domElement.style.top = '0px';
+labelRenderer.domElement.style.pointerEvents = 'none';
+document.body.appendChild(labelRenderer.domElement);
+
+window.addEventListener('resize', function() {
+  camera.aspect = window.innerWidth / this.window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  labelRenderer.setSize(this.window.innerWidth, this.window.innerHeight);
+});
+
+//-------------------------------------------------------------------
 
 
-//Botón para avanzar al paso siguiente
+// Botón para avanzar al paso siguiente
 const nextStepButton = document.createElement('button');
 document.getElementById("Next").append(nextStepButton);
 nextStepButton.textContent = 'Siguiente';
@@ -55,7 +123,7 @@ nextButton.addEventListener('click', function(e) {stepButtonVar();});
 
 
 
-//Botón para volver al inicio
+// Botón para volver al inicio
 const resetButton = document.createElement('button');
 document.getElementById("Reset").append(resetButton);
 resetButton.textContent = 'Inicio';
@@ -91,6 +159,7 @@ resButton.addEventListener('click', function(e) {resetButtonVar();});
 
 
 
+// Loader de modelos 3D en formato GLTF
 const GLoader = new GLTFLoader();
 GLoader.load("assets/prueba_ensamble_anim.glb", function (gltf) {
 
@@ -126,95 +195,27 @@ GLoader.load("assets/prueba_ensamble_anim.glb", function (gltf) {
     function(xhr) {console.log(( xhr.loaded/xhr.total * 100 ) + '%loaded');},
     function(error) {console.log('An error happened');}
 );
+//-----------------------------------------------------------------------
 
-/*const paragraph = document.createElement('paragraph');
-paragraph.textContent = '';
-const cPointLabel = new CSS2DObject(paragraph);
-scene.add(cPointLabel);
-cPointLabel.position.set(0, -0.45, 0);*/
 
-const labelRenderer = new CSS2DRenderer();
-labelRenderer.setSize(window.innerWidth, window.innerHeight);
-labelRenderer.domElement.style.position = 'absolute';
-labelRenderer.domElement.style.top = '0px';
-labelRenderer.domElement.style.pointerEvents = 'none';
-document.body.appendChild(labelRenderer.domElement);
 
-const planeGeo = new THREE.PlaneGeometry(30, 30);
-const planeMat = new THREE.MeshPhongMaterial({color: 0XFFFFFF, shininess:1200})
-const plane1 = new THREE.Mesh(planeGeo, planeMat);
-plane1.rotateX(Math.PI/-2);
-scene.add(plane1);
-plane1.receiveShadow = true;
-
-const aspect = w/h;
-const fov = 50;
-const near = 0.1;
-const far = 50;
-const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-camera.position.x = -4;
-camera.position.y = 4;
-camera.position.z = 3;
-
-//const scene = new THREE.Scene();
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.dampingFactor = 0.08;
-const geo = new THREE.IcosahedronGeometry(1.0, 2);
-const mat = new THREE.MeshStandardMaterial( {color: 0xffffff, flatShading: true });
-const mesh = new THREE.Mesh(geo, mat);
-//scene.add(mesh);
-mesh.position.y = 2.0;
-mesh.castShadow = true;
-
-const wireMat = new THREE.MeshBasicMaterial( { color: 0xccff, wireframe:true } );
-const wireMesh = new THREE.Mesh(geo, wireMat);
-wireMesh.scale.setScalar(1.001);
-//mesh.add(wireMesh);
-
-const dirLight = new THREE.DirectionalLight(0xFFFFFF, 2.0);
-const dirLightHelper = new THREE.DirectionalLightHelper(dirLight);
-dirLight.castShadow = true;
-dirLight.position.x = 0.5;
-dirLight.position.y = 3.1;
-dirLight.position.z = 1.3;
-dirLight.rotateY(-1.0);
-dirLight.rotateZ(-0.8);
-
-scene.add(dirLight);
-scene.add(dirLightHelper);
-
-const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff);
-scene.add(hemiLight);
-
-//const ambientLight = new THREE.AmbientLight(0xffffff);
-//ambientLight.castShadow = true;
-//scene.add(ambientLight);
-
-renderer.setClearColor(0xB0C4DE, 0.5);
-
-animate();
+//Función "animate" y llamado a la función para poner todo en marcha
 
 function animate(t = 0)
 {
-  
   requestAnimationFrame(animate);
 
-  // mesh.rotation.y = t*0.0001;
+  //mesh.rotation.y = t*0.0001;
 
   labelRenderer.render(scene, camera);
 
   renderer.render(scene, camera);
 
-  mixer.update(1/20);
+  mixer.update(1/15);
 
   controls.update();
-
 }
 
-window.addEventListener('resize', function() {
-  camera.aspect = window.innerWidth / this.window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  labelRenderer.setSize(this.window.innerWidth, this.window.innerHeight);
-});
+animate();
+
+//-------------------------------------------------------------------------
